f91e36121837820d480a6794e9c92b1ebaa74d86
thanhdat77 <nguyenthanhdat070701@gmail.com>
2021-12-10 17:54:22 +0700

    commit

--- 19494791_NguyenThanhDat_AD_HW2/app/src/main/java/com/example/a19494791_nguyenthanhdat_ad_todoapp/MainActivity.kt
+++ 19494791_NguyenThanhDat_AD_HW2/app/src/main/java/com/example/a19494791_nguyenthanhdat_ad_todoapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.a19494791_nguyenthanhdat_ad_todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,8 +22,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var todolist = mutableListOf(
+            toDo("làm bài tập","now",false) ,
+            toDo("nộp bài","tomorow",false)
+
+        )
         val addContactDialog = AlertDialog.Builder(this)
-            .setTitle("add contact")
+            .setTitle("add firebase")
 
             .setPositiveButton("SAVE"){dialogInterface,i->
                 Toast.makeText(this,"success",Toast.LENGTH_SHORT).show()
@@ -30,8 +37,21 @@ class MainActivity : AppCompatActivity() {
             .setNegativeButton("CANCEL"){dialogInterface,i->
                 Toast.makeText(this,"no success",Toast.LENGTH_SHORT).show()
             }.create()
+
+
+        val adapter = toAdapter(todolist)
+        ryview.adapter = adapter
+        ryview.layoutManager = LinearLayoutManager(this)
+
+
         bntUploadData.setOnClickListener(){
             addContactDialog.show()
+            val title = input.text.toString()
+            val todo = toDo(title,title,false)
+            todolist.add(todo )
+            adapter.notifyItemInserted(todolist.size-1)
+            val context = input.text.toString()
+            savetoDo(todo)
         }
     }
     private fun savetoDo(todo: toDo)= CoroutineScope(Dispatchers.IO).launch {
@@ -46,5 +66,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
 }
 
--- 19494791_NguyenThanhDat_AD_HW2/app/src/main/java/com/example/a19494791_nguyenthanhdat_ad_todoapp/toAdapter.kt
+++ 19494791_NguyenThanhDat_AD_HW2/app/src/main/java/com/example/a19494791_nguyenthanhdat_ad_todoapp/toAdapter.kt
@@ -1,8 +1,32 @@
 package com.example.a19494791_nguyenthanhdat_ad_todoapp
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.item_todo.view.*
 
-class toAdapter {
+
+class toAdapter(
+    var todos:List<toDo>
+) : RecyclerView.Adapter<toAdapter.todoViewHolder>() {
     inner class todoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): todoViewHolder {
+    var view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo,parent,false)
+        return todoViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+
+    override fun onBindViewHolder(holder: todoViewHolder, position: Int) {
+        holder.itemView.apply {
+            textView.text = todos[position].context
+
+        }
+    }
+
 }
